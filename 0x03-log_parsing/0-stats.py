a18d28e42cedb9a01c962d042f4a259143946bdc
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-06-19 17:24:47 +0300

    restoring type annotations

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Module 101-stats:
+"""Module 101-stats:
     Processes input in the format:
         <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> \
                , defaultdict <file size>
@@ -14,9 +13,10 @@ Module 101-stats:
             Status codes should be printed in ascending order.
 """
 from sys import stdin
+from typing import Dict, List, Tuple
 
 
-def print_stats(df, size):
+def print_stats(df: Dict[int, int], size: int) -> None:
     """Prints the statistics from stdin according to the format.
     """
     print("File size: {size}".format(size=size))
@@ -25,21 +25,21 @@ def print_stats(df, size):
             print("{k}: {v}".format(k=k, v=v))
 
 
-def init_vars():
+def init_vars() -> Tuple[Dict[int, int], Dict[str, int]]:
     """Initializes the variables to be used in the program.
     """
-    accumulator = {'size': 0, 'count': 0}
-    df = dict.fromkeys([200, 301, 400, 401,
+    accumulator: Dict[str, int] = {'size': 0, 'count': 0}
+    df: Dict[int, int] = dict.fromkeys([200, 301, 400, 401,
                                         403, 404, 405, 500],
                                        0)
     return df, accumulator
 
 
-def update_dict(line, df, accumulator):
+def update_dict(line: str, df: Dict, accumulator: Dict[str, int]) -> None:
     """Updates the dictionary and counts based on the input line.
         Every 10 lines, it prints the statistics.
     """
-    parts = line.split()
+    parts: List[str] = line.split()
     try:
         accumulator['size'] += int(parts[-1])
         if int(parts[-2]) in df:
@@ -51,7 +51,7 @@ def update_dict(line, df, accumulator):
         print_stats(df, accumulator['size'])
 
 
-def update_stats():
+def update_stats() -> None:
     """Reads lines from stdin and updates the statistics accordingly.
         Prints the statistics every 10 lines and on keyboard interruption.
     """
