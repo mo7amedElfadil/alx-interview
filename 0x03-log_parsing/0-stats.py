42b60cf6e70bdc412c4312913c16a65744181f03
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-06-21 01:35:20 +0300

    testing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,68 +1,56 @@
 #!/usr/bin/python3
-"""Module 101-stats:
-    Processes input in the format:
-        <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> \
-               , defaultdict <file size>
-    Every 10 lines and after a keyboard interruption (CTRL + C),
-    it prints statistics:
-        - Total file size: File size: <total size>
-            which is the sum of all previous sizes (see input format above)
-        - Number of lines by status code:
-            Possible status codes: 200, 301, 400, 401, 403, 404, 405, 500
-            Format: <status code>: <number>
-            Status codes should be printed in ascending order.
 """
-from typing import Dict, List, Tuple
+0. Log parsing
+"""
+import sys
+
+acc = {'file_size': 0,
+       'status_code': {200: 0, 301: 0, 400: 0, 401: 0,
+                       403: 0, 404: 0, 405: 0, 500: 0}}
 
 
-def print_stats(df: Dict[int, int], size: int) -> None:
-    """Prints the statistics from stdin according to the format.
+def print_log():
     """
-    print("File size: {size}".format(size=size))
-    for k, v in df.items():
-        if v:
-            print("{k}: {v}".format(k=k, v=v))
-
-
-def init_vars() -> Tuple[Dict[int, int], Dict[str, int]]:
-    """Initializes the variables to be used in the program.
+    This function prints the statistics from input read
     """
-    accumulator = {'size': 0, 'count': 0}
-    df = dict.fromkeys([200, 301, 400, 401,
-                        403, 404, 405, 500],
-                       0)
-    return df, accumulator
+    print('File size: {}'.format(acc['file_size']))
+    for key in sorted(list(acc['status_code'].keys())):
+        if acc['status_code'][key]:
+            print('{}: {}'.format(key, acc['status_code'][key]))
 
 
-def update_dict(line: str, df: Dict, accumulator: Dict[str, int]) -> None:
-    """Updates the dictionary and counts based on the input line.
-        Every 10 lines, it prints the statistics.
+def processer(line):
     """
-    parts = line.split()
+    This is the main engine which process the lines read
+
+    Parameters:
+    ----------
+    line: str
+        The processed line
+    """
+    size = line.split()[-1]
+    status = line.split()[-2]
+    if int(status) in list(acc['status_code'].keys()):
+        acc['status_code'][int(status)] += 1
+        acc['file_size'] += int(size)
+        return True
+    return False
+
+
+def main():
+    """
+    The entry point of the app
+    """
+    count = 0
     try:
-        accumulator['size'] += int(parts[-1])
-        if int(parts[-2]) in df:
-            df[int(parts[-2])] += 1
-            accumulator['count'] += 1
-    except (IndexError, ValueError):
-        pass
-    if accumulator['count'] % 10 == 0:
-        print_stats(df, accumulator['size'])
+        for line in sys.stdin:
+            if processer(line):
+                count += 1
+            if (count % 10 == 0):
+                print_log()
+    except KeyboardInterrupt or EOFError:
+        print_log()
 
 
-def update_stats() -> None:
-    """Reads lines from stdin and updates the statistics accordingly.
-        Prints the statistics every 10 lines and on keyboard interruption.
-    """
-    from sys import stdin
-    df, accumulator = init_vars()
-    try:
-        for line in stdin:
-            update_dict(line, df, accumulator)
-        print_stats(df, accumulator['size'])
-    except KeyboardInterrupt:
-        print_stats(df, accumulator['size'])
-
-
-if __name__ == "__main__":
-    update_stats()
+if __name__ == '__main__':
+    main()
